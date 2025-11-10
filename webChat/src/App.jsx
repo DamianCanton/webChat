2456c83a3195440b55638815d1090c8db7f65b75
Damian <damiancanton278@gmail.com>
2025-11-10 16:28:06 -0600

    Creacion de proyecto y de estilos globales

---
+++ webChat/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <body>
+      <h1>Web Chat Application</h1>
+    </body>
+  )
+}
+
+export default App
